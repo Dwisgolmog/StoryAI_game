7bd1de2fcc135c8fac2b669711fdf301c50d9893
DongHyun <95118426+Dwisgolmog@users.noreply.github.com>
2023-08-16 22:17:44 +0900

    프로젝트 시작

---
+++ storyAI/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(8080,function(){
+    console.log('listening on 8080');
+});
+
+app.use(express.static(path.join(__dirname,'client/build')));
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'client/build/index.html'));
+})
