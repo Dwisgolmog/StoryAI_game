436d08233528e9496522a5ffb7d6ace1e3e64f68
Dwisgolmog <rlaehdgus134@naver.com>
2023-08-21 23:55:40 +0900

    gpt 연결후 화면에 보여지기

--- storyAI/client/src/page/Game.js
+++ storyAI/client/src/page/Game.js
@@ -3,7 +3,7 @@ import { Box } from "@mui/system";
 import { Grid } from "@mui/material";
 import { Button,createTheme,ThemeProvider } from "@mui/material";
 import axios from "axios";
-import { useEffect,useState } from "react";
+import { React,Fragment,useEffect,useState } from "react";
 
 let theme = createTheme({});
 
@@ -17,21 +17,43 @@ theme = createTheme(theme, {
       }),
     },
   });
-
+//server 에서 get 요청한 result.data[0].content 의 값을 바인딩하기
 function Game(){
 
-    const[data,setData] = useState();
+    const[data,setData] = useState([]);
+    const[translate,setTranslate] = useState([]);
+    const[inputText,setInputText] = useState();
+    const[reload,setReload] = useState(0);
 
-    // /server/kogpt 라는 주소에 get 요청을 하여 kogpt의 답변을 가져옴
+    // /server/gpt 라는 주소에 get 요청을 하여 gpt의 답변을 가져옴
     useEffect(()=>{
-        axios.get('http://localhost:8080/server/kogpt')
+        axios.get('http://localhost:8080/server/gpt')
         .then(result=>{
-            console.log('axios 결과값:'+result.data.generations[0].text);
-            setData(result.data.generations[0].text);
+            console.log(result.data);
+
+            let copy = [...result.data];
+            console.log("copy:"+copy);
+            setData(copy);
         }).catch(e=>{
             console.log('axios 에러발생!!:'+e);
         })
-    },[])
+    },[reload])
+
+    //429오류 발생
+    // useEffect(()=>{
+    //     axios.post('http://localhost:8080/server/translate',{textToTranslate:'hello world'})
+    //     .then((response)=>{
+    //         console.log(response.data);
+    //     })
+    //     .catch(e=>{
+    //         console.log('/server/translate 클라이언트 post error!:'+e);
+    //     })
+    // },[data])
+
+    //input값을 저장하는 함수
+    const handleChange = e=>{
+        setInputText(e.target.value);
+    }
 
     return(
         <>
@@ -43,17 +65,24 @@ function Game(){
                             height:'35vw',
                             bgcolor:'white',
                             border:'2px solid gray',
-                            p:2
+                            p:2,
+                            overflow: "hidden",
+                            overflowY: "scroll",
                             }} >
                                 {
-                                    
+                                    data[1] != null ?
+                                    data.map((item,index)=>(
+                                        index >=2 &&
+                                        <p key={index}>{item.content.split('\n').map((text, index) => (
+                                            <Fragment key={index}>
+                                                {text}
+                                                <br />
+                                            </Fragment>
+                                        ))}</p>
+                                    ))
+                                    : null
                                 }
-                                <p>
-                                    진행자:좀비 아포칼립스에 오신걸 환영합니다.
-                                </p>
-                                <p>
-                                    Data: {data}
-                                </p>
+                                
 
                             </Box>
                         </Grid>
@@ -71,11 +100,22 @@ function Game(){
                     </Grid>
                     <Grid container>
                             <Grid item sx={{mt:4}} xs={8.6}>
-                                <input style={{width:'100%',height:'5vw'}}></input>
+                                <input onChange={handleChange} style={{width:'100%',height:'5vw'}}></input>
                             </Grid>
                             <Grid item xs={0.4}/>
                             <Grid item sx={{mt:4}} xs={3}>
-                                <Button variant="contained" color="enter" style={{width:'100%',height:'5vw'}}>Enter</Button>
+                                {/* button을 누를시 답변을 post 요청하고 setReload로 페이지를 재랜더링함 */}
+                                <Button onClick={()=>{
+                                    axios.post('http://localhost:8080/server/gpt/send',{inputText:inputText})
+                                    .then((response)=>{
+                                        console.log("response:"+response);
+                                        setReload(reload+1);
+                                    }).catch(e=>{
+                                        console.log('inputText post error=======>'+e);
+                                    })
+                                }
+                                    //null
+                                } variant="contained" color="enter" style={{width:'100%',height:'5vw'}}>Enter</Button>
                             </Grid>
                     </Grid>
 
--- storyAI/server.js
+++ storyAI/server.js
@@ -22,9 +22,10 @@ MongoClient.connect('mongodb+srv://dnwn3027:suwon1234@stone.jf0hj90.mongodb.net/
 const clientId = 'exJJisjHTM4Hro9BlAc8';
 const clientSecret = 'EoC1NjPiy3';
 // 번역할 텍스트와 언어 코드들
-const textToTranslate = 'Hello, world!';
+//const textToTranslate = 'Hello, world!';
 const sourceLang = 'en';
 const targetLang = 'ko';
+
 // 번역 요청을 위한 URL
 const apiUrl = 'https://openapi.naver.com/v1/papago/n2mt';
 
@@ -34,11 +35,11 @@ const headers2 = {
   'X-Naver-Client-Id': clientId,
   'X-Naver-Client-Secret': clientSecret,
 };
+
 // 번역 요청 데이터
 const data2 = new URLSearchParams();
 data2.append('source', sourceLang);
 data2.append('target', targetLang);
-data2.append('text', textToTranslate);
 
 
     
@@ -48,9 +49,12 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'client/build/index.html'));
 })
 
+
+
 // Papago API 호출
-app.get('/server/translate', function (req, res) {
-  console.log('연동되었습니다')
+app.post('/server/translate', function (req, res) {
+  console.log('연동되었습니다') 
+  data2.append('text','hello world' );
   axios.post(apiUrl, data2, { headers: headers2 })
     .then(response => {
       const translatedText = response.data.message.result.translatedText;
@@ -58,70 +62,71 @@ app.get('/server/translate', function (req, res) {
       res.send(`Translated Text: ${translatedText}`);
     })
     .catch(error => {
-      console.error('Error during translation:', error);
+      console.error('파파고 에러', error);
       res.status(500).send('Error during translation');
     });
 });
 
+app.get('/server/translate/hi',function(req,res){
+  res.send('hi');
+})
+
+const chatMessages = [{
+  "role": "system",
+  "content": "Write a story and suggest simple questions (a/b/c choice or yes/no) to continue the next story.Story is changed from the answer. Wait before I answer the question. If i answer the question, continue the story. Repeat this steps and lead to happy ending after 7 turns.\n\n[Main Story]\n\nTopic: find a hope to save the earth\n\nMain Character: poor little chiwawa dog in a town\n\n[Requirements]\n\nthis is for kids and english education for international students\n\neach story around 100 words length.\n\nonly use basic 300 vocaburary."
+},
+{
+  "role": "user",
+  "content": ""
+},
+
+];
 
 //chat gpt key 
-// const gptKeyValue = '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';
+const gptKeyValue = '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';
 
-// // gpt 연결
-// const OpenAI = require('openai');
-
-// const openai = new OpenAI({
-//   apiKey: gptKeyValue
-// });
-
-// openai.chat.completions.create({
-//   model: "gpt-3.5-turbo",
-//   messages: [
-//     {
-//       "role": "system",
-//       "content": "당신은 도움이되는 조수입니다."
-//     },
-//     {
-//       "role": "user",
-//       "content": ""
-//     }
-//   ],
-//   temperature: 1,
-//   max_tokens: 256,
-//   top_p: 1,
-//   frequency_penalty: 0,
-//   presence_penalty: 0,
-// })
-// .then(chatCompletion => {
-//   console.log(chatCompletion.choices[0].message);
-// })
-// .catch(error => {
-//   console.error(error);
-// });
-
-
-
-// axios로 chatgpt에 get 요청을 하여 데이터를  받아옴
-app.get('/server/kogpt', function (req, res) {
-  console.log('running api');
-  try {
-    axios.post(url, gptData, { headers })
-      .then(response => {
-        console.log('요청 성공!');
-        console.log('Response:', response.data);
-
-        res.send(response.data);
-      })
-      .catch(error => {
-        console.error(`서버 axios 에러: ${error}`);
-        res.status(500).send('서버 axios 에러');
-      });
-  } catch (error) {
-    console.error(`서버 try문 에러: ${error}`);
-    res.status(500).send('서버 try문 에러');
-  }
+// gpt 연결
+const OpenAI = require('openai');
+const openai = new OpenAI({
+  apiKey: gptKeyValue
 });
+
+app.get('/server/gpt',function(req,res){
+  openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: chatMessages, //데이터 가져오기(DB처리 필요)
+    temperature: 1,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  })
+  .then(chatCompletion => {
+    let result = chatCompletion.choices[0].message;
+    //chatMessages에 데이터 추가 (DB처리 필요)
+    chatMessages.push(result);
+    console.log("server newChatMessage:"+JSON.stringify(result));
+    res.send(chatMessages); //데이터 가져와서 보내기 (DB처리 필요)
+  })
+  .catch(error => {
+    console.error(error);
+  });
+  
+})
 
 app.get('*',function(req,res){
     res.sendFile(path.join(__dirname,'client/build/index.html'));
+
+})
+
+//Game.js에서 post 요청시 input 박스안에 있는 값(답변)을 chatMessages에 저장함
+app.post('/server/gpt/send',function(req,res){
+  const userAnswer = {
+    "role": "user",
+    "content":req.body.inputText, 
+  };
+  //chatMessages에 데이터 추가 (DB처리 필요)
+  chatMessages.push(userAnswer);
+  console.log('chatMessages=====================\n'+JSON.stringify(chatMessages)+"\n\n");
+  res.sendStatus(200)
 })
