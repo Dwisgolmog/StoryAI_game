33ff84be0920fc02b0cbe112a11a32ce9e076060
dnwn1211 <dnwn3027@gmail.com>
2023-11-24 00:03:04 +0900

    navbar
    navbar

--- storyAI/client/src/Components/NavBar.js
+++ storyAI/client/src/Components/NavBar.js
@@ -1,57 +1,19 @@
-import { React,useState,useEffect } from 'react';
-import { Container, Grid } from '@mui/material';
 import '../App.css';
-import Toolbar from '@mui/material/Toolbar';
-import { NavLink } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function NavBar({bgColor}) {
-    const [color,setColor] = useState(bgColor);
-
-    useEffect(()=>{
-        if(color=='white'){
-            setColor('black');
-        }else{
-            setColor('rgb(240, 240, 240)');
-        }
-    },[bgColor])
-
+function NavBar() {
     return (
-        <>
-            <Container maxWidth="xl" sx={{mt:0, pt: 2}} position="static" color='default' className='navBar' style={{backgroundColor:color,width:'100%'}}>
-                    <Toolbar disableGutters>
-                        <Grid container item xs={8} className='leftNav' style={{color:bgColor}}>
-                            <Grid item xs={4}>
-                                <NavLink to={'/'}>
-                                {
-                                    bgColor == 'black' ? <img src='/img/WhiteLogo.png' />
-                                    : <img src='/img/WhiteLogo2.png' style={{width:'8vw', height:'5vw' }}></img>
-                                }
-                                </NavLink>
-                            </Grid>
-                            <Grid item xs={4}>
-                                Learn About Us
-                            </Grid>
-                            <Grid item xs={4}>
-                                Game Select
-                            </Grid>
-                        </Grid>
-
-                        <Grid container item xs={4} className='rightNav' style={{color:bgColor}}>
-                            <Grid item xs={4}>
-                                Log In
-                            </Grid>
-                            <Grid item xs={4}>
-                                Save
-                            </Grid>
-                            <Grid item xs={4}>
-                                Load
-                            </Grid>
-                        </Grid>
-                    </Toolbar>
-    
-            </Container>
-        </>
+        <div className="flex justify-center items-center bg-white-800 text-black p-4">
+            <ul className="flex space-x-4">
+                <li><img src='/img/WhiteLogo.png' className="w-29 h-16" alt="Logo" /></li>
+                <li><a href='#' className="hover:underline py-16">About Learn US</a></li>
+                <li><a href='#' className="hover:underline py-16">Game Select</a></li>
+                <li><Link to='/Login'className="hover:underline py-16">Login</Link></li>
+                <li><a href='#' className="hover:underline py-16">Save</a></li>
+                <li><a href='#' className="hover:underline py-16">Load</a></li>
+            </ul>
+        </div>
     );
 }
 
-export default NavBar
+export default NavBar;
